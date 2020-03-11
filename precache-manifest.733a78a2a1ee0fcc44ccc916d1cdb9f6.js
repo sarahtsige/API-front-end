4ec6fffd86cc6db1518eafc9a95be4af6af8c8a3
sarahsaleh <sarahtsige@gmail.com>
2020-03-10 20:14:05 -0400

    Updates

---
+++ precache-manifest.733a78a2a1ee0fcc44ccc916d1cdb9f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed3fb85e94d2fb45581ad6d7201530e",
+    "url": "/api-front-end/index.html"
+  },
+  {
+    "revision": "cce113ace2b8c9ef2fd6",
+    "url": "/api-front-end/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b45c8bbedd7bf8a79f6d",
+    "url": "/api-front-end/static/css/main.3452b02d.chunk.css"
+  },
+  {
+    "revision": "cce113ace2b8c9ef2fd6",
+    "url": "/api-front-end/static/js/2.8b54d28d.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/api-front-end/static/js/2.8b54d28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45c8bbedd7bf8a79f6d",
+    "url": "/api-front-end/static/js/main.a7d4d569.chunk.js"
+  },
+  {
+    "revision": "366a3e78ff0b9348f1de",
+    "url": "/api-front-end/static/js/runtime-main.dd9371f6.js"
+  },
+  {
+    "revision": "5f8acf787006edef1c6c40342329a708",
+    "url": "/api-front-end/static/media/map-of-the-world-2401458_1920.5f8acf78.jpg"
+  }
+]);
